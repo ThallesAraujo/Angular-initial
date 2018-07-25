0038ff6fde4fe902faa717be6826c729a8621fb6
Thalles Araújo <thalleshenrique.na@gmail.com>
2018-07-25 13:01:16 -0300

    Adição de combobox de seleção de gênero (preliminar)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,14 +9,17 @@ import { Component } from '@angular/core';
 export class AppComponent {
   nome = '';
   adicionado = false;
+  sexo = '';
   ultimoId = 0;
   funcionarios = [];
 
-  adicionar(nome : string){
+  adicionar(nome : string, sexo: string){
     this.nome = nome;
     this.adicionado = true;
+    this.sexo = sexo;
     this.funcionarios.push({
       nome: this.nome,
+      sexo : this.sexo,
       id: ++ this.ultimoId,
     });
   }
