71094f2dfc4e7933dae92a3d54aa9ae48e73a06b
Thalles Araújo <thalleshenrique.na@gmail.com>
2018-08-09 20:05:51 -0300

    Registro de FormControls (M12a03)

--- src/app/formularios/formularios.component.ts
+++ src/app/formularios/formularios.component.ts
@@ -10,6 +10,9 @@ export class FormulariosComponent{
 
   salvar(form: NgForm){
     console.log(form);
+    /*Por exemplo: caso se quisesse pegar o nome:
+    form.value.nome
+    modelo-> form.value.nomeDaPropriedadeNoTemplate*/
   }
 
 }
